c5758c468d2adba77aa629a46b512e88302a8f60
Tomas Utreras <tutreras100@gmail.com>
2021-02-27 02:25:54 -0500

    button.js EC.js NavBar.js API.js

---
+++ src/components/EmployeeContext.js
@@ -0,0 +1,44 @@
+import React, { useState, useContext } from 'react';
+import { useGet } from "../hooks/API.js"
+import "./Table.css"
+import Button from "./Button.js"
+import { EmployeeContext } from './EmployeeContext.js';
+
+
+function Table() {
+   
+    const [url] = useState("https://randomuser.me/api/?results=50&nat=us")
+    const { sortFunc } = useGet(url);
+    const { displayedEmployees} = useContext(EmployeeContext)
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <td onClick={() => sortFunc("name")}><Button>First Name</Button></td>
+                    <td>Last Name</td>
+                    <td>UID</td>
+                    <td>Gender</td>
+                    <td>E-mail</td>
+                    <td onClick={() => sortFunc("age")}><Button>Age</Button></td>
+                </tr>
+            </thead>
+            <tbody>
+                {displayedEmployees.map(employee => {
+                    return (
+                        <tr key={employee.login.uuid}>
+                            <td>{employee.name.first}</td>
+                            <td>{employee.name.last}</td>
+                            <td>{employee.login.uuid}</td>
+                            <td>{employee.gender}</td>
+                            <td>{employee.email}</td>
+                            <td>{employee.dob.age}</td>
+                        </tr>
+                    )
+                }
+                )}
+            </tbody>
+        </table>
+    )
+}
+export default Table;
---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Search from "./Search.js"
+import "./Navbar.css"
+
+function Navbar(){
+   
+    return(
+        <div className="navbar">
+            <div className="logo bold">Employee Directory</div>
+            <div className="descriptor">
+                <div className="bold">Employee Search: </div>
+                <Search />
+            </div>
+        </div>
+    )
+}
+export default Navbar;
