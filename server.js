ddce8d6c2b162e9aa77a9b0a3e91cb092f522823
Tomas Utreras <tutreras100@gmail.com>
2021-02-27 20:59:55 -0500

    try to deploy

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+
+const path = require("path")
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, "build")))
+
+app.get("/", function (req, res){
+    res.sendFile(path.join(__dirname,"build", "index.html"))
+})
+
+app.get('/*', function (req, res){
+    res.sendfile(path.join(__dirname, 'build', 'index.html'))
+})
+
+app.listen(process.env.POR || 8080)
