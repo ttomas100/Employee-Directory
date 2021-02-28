cb979ddb9d71bc44508e0b36ed27e0e3858b314e
Tomas Utreras <tutreras100@gmail.com>
2021-02-27 20:15:35 -0500

    final commit

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,9 @@
 import React, { useState, useContext } from 'react';
 import { useGet } from "../hooks/API.js"
+import "./Table.css"
 import Button from "./Button.js"
 import { EmployeeContext } from './EmployeeContext.js';
-import "./Table.css"
+
 
 function Table() {
    
